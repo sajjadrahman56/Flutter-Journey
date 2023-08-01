bf334745da405aedf215df1d5942482102409ae7
sajjad-njr <maxsajjad29@gmail.com>
2023-08-01 23:17:30 +0600

    self path add

---
+++ Dart Language/code/self-learn/prectice.dart
@@ -0,0 +1,8 @@
+void main() {
+  // print('Hello VS Code! Hello Dart!');
+  String firstName = 'sajjad';
+  String lastName = 'hossain';
+  print(firstName + ' ' + lastName);
+  print(firstName.runtimeType);
+  print('my firstname is $firstName and lastname is $lastName');
+}
