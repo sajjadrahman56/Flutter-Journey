f5f45b9d66019fbaac4954f13aab91d65046d0e9
sajjad-njr <maxsajjad29@gmail.com>
2023-08-12 23:31:51 +0600

    function rewrite

--- Dart Language/code/9-functions/1_fun.dart
+++ Dart Language/code/9-functions/1_fun.dart
@@ -1,12 +1,7 @@
 void main() {
   welcome();
-  print(add(45, 56));
-}
-
-int add(int a, int b) {
-  return a + b;
 }
 
 welcome() {
-  print('welcome to the here');
+  print("Welcome to Dart Programming");
 }
