d492adca427079e15168de3fdf4a623f768ec9ea
sajjad-njr <maxsajjad29@gmail.com>
2023-08-17 23:17:37 +0600

    task -03 oop

---
+++ Tasks/task-03/cars.dart
@@ -0,0 +1,36 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+  double milesDriven;
+
+  static int numberOfCars = 0;
+
+  Car(this.brand, this.model, this.year, this.milesDriven) {
+    numberOfCars++;
+  }
+
+  void drive(double miles) {
+    milesDriven += miles;
+  }
+
+  double getMilesDriven() {
+    return milesDriven;
+  }
+
+  String getBrand() {
+    return brand;
+  }
+
+  String getModel() {
+    return model;
+  }
+
+  int getYear() {
+    return year;
+  }
+
+  int getAge() {
+    return 2023 - year;
+  }
+}
