e6f8464a478da719c9edda25f12482bcf3dc57dd
sajjad-njr <sajjadrahman1020@gmail.com>
2023-08-19 22:10:53 +0600

    pattern of number

--- Dart Language/problem-solvning/pattern/1.dart
+++ Dart Language/problem-solvning/pattern/1.dart
@@ -1,12 +1,70 @@
 import "dart:io";
 
 void main() {
-  pattern4(4);
+  pattern17(4);
   // pattern2(4);
   // pattern1(4);
 }
 
-void pattern4(int n) {}
+void pattern17(int n) {
+  for (int row = 1; row <= 2 * n; row++) {
+    int totalColumNo = row > n ? 2 * n - row : row;
+    int totalSpcae = n - row;
+
+    for (int k = 0; k < n - totalColumNo; k++) {
+      stdout.write("  ");
+    }
+    for (int col = totalColumNo; col >= 1; col--) {
+      stdout.write('$col' + " ");
+    }
+    for (int col = 2; col <= totalColumNo; col++) stdout.write("$col ");
+
+    print(' ');
+  }
+}
+
+void pattern30(int n) {
+  for (int row = 1; row <= n; row++) {
+    int totalSpcae = n - row;
+
+    for (int k = 0; k < n - row; k++) {
+      stdout.write("  ");
+    }
+    for (int col = row; col >= 1; col--) {
+      stdout.write('$col' + " ");
+    }
+    for (int col = 2; col <= row; col++) stdout.write("$col ");
+
+    print(' ');
+  }
+}
+
+void pattern28(int n) {
+  for (int row = 0; row < 2 * n; row++) {
+    int totalColumNo = row > n ? 2 * n - row : row;
+
+    int totalSpcae = n - totalColumNo;
+
+    for (int k = 0; k < totalSpcae; k++) {
+      stdout.write(' ');
+    }
+    for (int col = 0; col < totalColumNo; col++) {
+      stdout.write("* ");
+    }
+    print(' ');
+  }
+}
+
+void pattern4(int n) {
+  for (int row = 0; row < 2 * n; row++) {
+    int totalColumNo = row > n ? 2 * n - row : row;
+
+    for (int col = 0; col < totalColumNo; col++) {
+      stdout.write("* ");
+    }
+    print(' ');
+  }
+}
 
 void pattern3(int n) {
   //****
