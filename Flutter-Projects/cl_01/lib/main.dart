11a165b82b1ade80e5d19e7aa2c68f3b760cc9a0
sajjad-njr <maxsajjad29@gmail.com>
2023-09-04 23:20:52 +0600

    Widget Tree create , home screen divided into number of class

--- Flutter-Projects/cl_01/lib/main.dart
+++ Flutter-Projects/cl_01/lib/main.dart
@@ -1,77 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home is named prameter of MaterialApp. if small latter it is property of MaterialApp
+      // if capital latter it is class of MaterialApp
+      home: HomeScreen(),
+      title: 'MF app',
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        title: Text("My First App"),
+        leading: Icon(
+          Icons.home,
+          color: Colors.amber,
+          size: 40,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Text(
+            "Hello Screen 1",
+            style: TextStyle(
+              fontSize: 29,
+              color: Colors.red,
+              fontFamily: 'Lobster',
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Hello Screen 2",
+            style: TextStyle(
+              fontSize: 29,
+              color: Colors.red,
+              fontFamily: 'Lobster',
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
