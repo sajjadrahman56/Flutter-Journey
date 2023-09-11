11cd13a2d82d5adaeb4e6d49390fd617dbd139ae
sajjad-njr <maxsajjad29@gmail.com>
2023-09-11 23:35:27 +0600

    Grid view

--- Flutter-Projects/basic_widgets/lib/main.dart
+++ Flutter-Projects/basic_widgets/lib/main.dart
@@ -31,67 +31,26 @@ class HomeScree extends StatelessWidget {
         ),
         body: Center(
           child: Scrollbar(
-            thickness: 30,
-            child: ListView(
-              /// list of widgets
-              /// automatic scrool direction/view
-              ///  no main axis alignment or cross axis alignment
-              ///  We can not achive the task of row and column with listview
-              /// by defautl scroll direction is vertical but we can change it to horizontal
-              /// we can not use listview inside listview
-              ///
-
-              children: [
-                ListTile(
-                  title: Text('User Name'),
-                  subtitle: Text('user@gmail.com'),
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.edit),
-                  onTap: () {
-                    print('ListTile Tapped');
-                  },
+              thickness: 30,
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 7,
                 ),
-                ListTile(
-                  title: Text('User Name'),
-                  subtitle: Text('user@gmail.com'),
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.edit),
-                  onTap: () {
-                    print('ListTile Tapped');
-                  },
-                ),
-                Divider(
-                  color: Colors.red,
-                  thickness: 1,
-                ),
-                ListTile(
-                  title: Text('User Name'),
-                  subtitle: Text('user@gmail.com'),
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.edit),
-                  onTap: () {
-                    print('ListTile Tapped');
-                  },
-                ),
-                ListTile(
-                  title: Text('User Name'),
-                  subtitle: Text('user@gmail.com'),
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.edit),
-                  onTap: () {
-                    print('ListTile Tapped');
-                  },
-                ),
-                Text('Hello World'),
-                Text('Hello World'),
-                Text('Hello World'),
-                Text('Hello World'),
-                Text('Hello World'),
-                Text('Hello World'),
-                Text('Hello World'),
-              ],
-            ),
-          ),
+                children: [
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                ],
+              )),
         ));
   }
 }
