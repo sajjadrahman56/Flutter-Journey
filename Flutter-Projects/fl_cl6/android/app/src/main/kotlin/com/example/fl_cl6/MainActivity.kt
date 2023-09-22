2dd9db7d9320b216022f0b1318b3602ff48acdc7
sajjad-njr <maxsajjad29@gmail.com>
2023-09-22 22:02:15 +0600

    pr created

--- Flutter-Projects/fl_cl6/android/app/src/main/kotlin/com/example/fl_cl6/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fl_cl6
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ fl_cl_08_pr/android/app/src/main/kotlin/com/example/fl_cl_08_pr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fl_cl_08_pr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
